1eda238ee334bf8a79a4053430689313d3d51faa
Kenan Hu <khu@eqbank.ca>
2019-07-11 21:06:44 +0000

    trap impl

--- traproutines.cpp
+++ traproutines.cpp
@@ -3,7 +3,11 @@
 #include "memory.h"
 #include <stdint.h>
 #include <array>
+#include <iostream>
 
+
+#define INC_BY 1
+//#define INC_BY_UINT16
 
 TrapRoutines::TrapRoutines(): 
 routine(
@@ -25,32 +29,44 @@ TrapRoutines::~TrapRoutines(){
 
 void TrapRoutines::ExecuteTrapRoutine(uint16_t trap_code, Registers* registers, Memory* memory) const{
 	int trap_routine_index = trap_code - 0x20;
-	(this->*routine[trap_routine_index])((*registers)[Registers::R0], memory);
+	(this->*routine[trap_routine_index])(&(*registers)[Registers::R0], memory);
 }
 
 /*---------------------------------------------------Trap routine implementations----------------------------------------------------------*/
 
 
 void TrapRoutines::Trap_GETC(uint16_t* R0, Memory*) const{
-	char c = '';
+	char c = '\0';
 	std::cin>>c;
 	*R0 = (uint16_t)c;
 }
 
 void TrapRoutines::Trap_OUT(uint16_t* R0, Memory*) const{
-
+	std::cout<<(char)*R0;
 }
 
-void TrapRoutines::Trap_PUTS(uint16_t* R0, Memory*) const{
-
+void TrapRoutines::Trap_PUTS(uint16_t* R0, Memory* memory) const{
+	Memory& mem = *memory;
+#if INC_BY==1
+	std::string str((char*)(&mem[0]+*R0));
+#elif INC_BY==2
+	std::string str((char*)(&mem[*R0]));
+#endif
+	std::cout<<str;
 }
 
 void TrapRoutines::Trap_IN(uint16_t* R0, Memory*) const{
 
 }
 
-void TrapRoutines::Trap_PUTSP(uint16_t* R0, Memory*) const{
-
+void TrapRoutines::Trap_PUTSP(uint16_t* R0, Memory* memory) const{
+	Memory& mem = *memory;
+#if INC_BY==1
+	const uint16_t& unit = *(&mem[0]+*R0);
+#elif INC_BY==2
+	const uint16_t& unit = mem[*R0];
+#endif
+	std::cout<<(char)(unit&0xff)<<(char)(unit>>8&0xff);
 }
 
 void TrapRoutines::Trap_HALT(uint16_t*, Memory*) const{
